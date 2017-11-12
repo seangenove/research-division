e48db87fcffe9147456df9e14c37dcbadbd624df
Christopher Espiritu <christopheredrian@gmail.com>
2017-11-12 16:52:51 +0800

    Added api key for google maps api

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -651,7 +651,7 @@
 <script src="/js/bootstrap.min.js"></script>
 <script src="/js/jquery.flexslider.js"></script>
 <script src="/js/jquery.inview.js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD8HeI8o-c1NppZA-92oYlXakhDPYR7XMY"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC7dvhrXSpMj_XFOeDt3DgmWuDG6JHewb4"></script>
 <script src="/js/script.js"></script>
 <script src="/contactform/contactform.js"></script>
 
