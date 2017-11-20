c3d985bd5581c3552eb3f831c2de833d54a5c804
MarkEslao <maxwellrenard@gmail.com>
2017-11-20 20:45:57 +0800

    Modified the edit page function

--- resources/views/admin/pages/edit.blade.php
+++ resources/views/admin/pages/edit.blade.php
@@ -12,56 +12,48 @@
 @section('content')
     <div class="box box-default color-palette-box">
     <div class="box-header with-border">
-        <h3 class="box-title"><i class="fa fa-file-code-o"></i> New Page</h3>
-    </div>
+                <h3 class="box-title">Edit {{ $page->title }}</h3>
+            </div>
     <div class="box-body">
         <div>
             <a href="/admin/pages" class="btn btn-info">Back</a>
 
         </div>
-        <div class="box-header">
-            <h3 class="box-title">Page Title
-            </h3>
+        <form method="post" action="{{ url("/admin/pages/{$page->id}/") }}">
+                {{ method_field('PATCH') }}
+                {{ csrf_field() }}
+            <div class="box-header">
+                <h3 class="box-title">Page Title
+                </h3>
+                <div class="box-body pad">
+                    <input name="title" type="text" class="form-control" placeholder="Enter ..." value="{{ $page->title }}">
+                </div>
+            </div>
+            <div class="box-header">
+                <h3 class="box-title">Page Description
+                </h3>
+                <div class="box-body pad">
+                    <textarea name="description" class="form-control" rows="3" placeholder="Enter ...">{{ $page->description }}</textarea>
+                </div>
+            </div>
+            <div class="box-header">
+                <h3 class="box-title">Page Body
+                </h3>
+                <!-- tools box -->
+                <div class="pull-right box-tools">
+                </div>
+                <!-- /. tools -->
+            </div>
+            <!-- /.box-header -->
             <div class="box-body pad">
-                <form>
-                    <input type="text" class="form-control" placeholder="Enter ...">
-                </form>
-            </div>
-        </div>
-        <div class="box-header">
-            <h3 class="box-title">Page Description
-            </h3>
-            <div class="box-body pad">
-                <form>
-                    <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
-                </form>
-            </div>
-        </div>
-        <div class="box-header">
-            <h3 class="box-title">Page Header
-            </h3>
-            <div class="box-body pad">
-                <form>
-                    <input type="text" class="form-control" placeholder="Enter ...">
-                </form>
-            </div>
-        </div>
-        <div class="box-header">
-            <h3 class="box-title">Page Body
-            </h3>
-            <!-- tools box -->
-            <div class="pull-right box-tools">
-            </div>
-            <!-- /. tools -->
-        </div>
-        <!-- /.box-header -->
-        <div class="box-body pad">
-            <form>
-                <textarea id="editor1" name="editor1" rows="10" cols="80">
+                <textarea name="content" id="editor1" name="editor1" rows="10" cols="80" >
+                    {{ $page->content }}
                     </textarea>
-            </form>
-        </div>
-
+            </div>
+            <div class="box-footer">
+                    <button type="submit" class="pull-right btn btn-primary">Submit</button>
+                </div>
+        </form>
     </div>
 </div>
 @endsection
