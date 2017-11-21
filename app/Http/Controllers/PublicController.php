2f1797013229eb1ca0d9f0c51a728661177d618b
Charlene Fama <2150428@slu.edu.ph>
2017-11-21 22:41:50 +0800

    created dynamic list of resolutions

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -4,52 +4,64 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\LogUtility;
-
+use DB;
 
 class PublicController extends Controller
 {
-    public function index() {
-        LogUtility::insertLog("HttpRequest on /",'public');
+    public function index()
+    {
+        LogUtility::insertLog("HttpRequest on /", 'public');
         return view('public.index');
     }
 
-    public function resolution() {
-        LogUtility::insertLog("HttpRequest on /resolution",'public');
-        return view('public.resolution');
+    public function resolution()
+    {
+        LogUtility::insertLog("HttpRequest on /resolution", 'public');
+
+        $resolutions = DB::table('resolutions')->get();
+
+        return view('public.resolution', ['resolutions' => $resolutions]);
     }
 
-    public function ordinance() {
-        LogUtility::insertLog("HttpRequest on /ordinance",'public');
+    public function ordinance()
+    {
+        LogUtility::insertLog("HttpRequest on /ordinance", 'public');
         return view('public.ordinance');
     }
 
-    public function monitorAndEval() {
-        LogUtility::insertLog("HttpRequest on /monitorAndEval",'public');
+    public function monitorAndEval()
+    {
+        LogUtility::insertLog("HttpRequest on /monitorAndEval", 'public');
         return view('public.monitorAndEval');
     }
 
-    public function about() {
-        LogUtility::insertLog("HttpRequest on /about",'public');
+    public function about()
+    {
+        LogUtility::insertLog("HttpRequest on /about", 'public');
         return view('public.about');
     }
 
-    public function contactUs() {
-        LogUtility::insertLog("HttpRequest on /contactUs",'public');
+    public function contactUs()
+    {
+        LogUtility::insertLog("HttpRequest on /contactUs", 'public');
         return view('public.contactUs');
     }
 
-    public function aboutDiv() {
-        LogUtility::insertLog("HttpRequest on /aboutDiv",'public');
+    public function aboutDiv()
+    {
+        LogUtility::insertLog("HttpRequest on /aboutDiv", 'public');
         return view('public.aboutDiv');
     }
 
-    public function reports() {
-        LogUtility::insertLog("HttpRequest on /reports",'public');
+    public function reports()
+    {
+        LogUtility::insertLog("HttpRequest on /reports", 'public');
         return view('public.reports');
     }
 
-    public function showResolution() {
-        LogUtility::insertLog("HttpRequest on /showResolution",'public');
+    public function showResolution()
+    {
+        LogUtility::insertLog("HttpRequest on /showResolution", 'public');
         return view('public.showResolution');
     }
 }
