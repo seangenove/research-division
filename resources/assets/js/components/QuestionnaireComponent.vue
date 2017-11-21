aa81df31db336956e42873036922da9974430e97
Christopher Espiritu <christopheredrian@gmail.com>
2017-11-22 00:08:45 +0800

    Variable naming on migrations and vue component and housekeeping on layout

--- resources/assets/js/components/QuestionnaireComponent.vue
+++ resources/assets/js/components/QuestionnaireComponent.vue
@@ -3,14 +3,14 @@
         <div class="form-group">
             <label for="questionnaireName">Questionnaire Name</label>
             <input class="form-control" id="questionnaireName" v-model="questionnaire.name" type="text"
-                   placeholder="Question name">
+                   placeholder="Questionnaire Name...">
         </div>
         <div class="form-group">
             <label>Description</label>
             <textarea class="form-control" v-model="questionnaire.description" id="" cols="30" rows="10"></textarea>
         </div>
         <hr>
-        <button v-on:click="addQuestion()">Add Question</button>
+        <button v-on:click="addQuestion()" class="btn btn-success btn-sm">Add Question</button>
         <div v-for="question in questionnaire.questions">
             <div class="row">
                 <div class="col-md-2"></div>
@@ -20,12 +20,12 @@
                         <input class="form-control" type="text" v-model="question.question">
                     </div>
                     <div class="form-group">
-                        <input type="checkbox" v-model="question.isRequired" class="check">
+                        <input type="checkbox" v-model="question.required" class="check">
                         <label>Required</label>
                     </div>
                     <div class="form-group">
                         <label for="">Answer Type</label>
-                        <select v-model="question.answerType" id="quesType" class="form-control"
+                        <select v-model="question.type" id="quesType" class="form-control"
                                 v-on:change="question.values = []">
                             <option v-bind:value="'short'">Short Answer</option>
                             <option v-bind:value="'long'">Long Answer</option>
@@ -33,19 +33,19 @@
                             <option v-bind:value="'checkbox'">Checkboxes</option>
                         </select>
                     </div>
-                    <div v-if="question.answerType === 'radio'" class="form-group">
+                    <div v-if="question.type === 'radio'" class="form-group">
                         <label for="">Radio Box Values</label>
+                        <button v-on:click="addValue(question)" class="btn btn-success btn-xs">Add Value</button>
                         <div v-for="val in question.values">
                             <input class="form-control" type="text" v-model="val.value" required>
                         </div>
-                        <button v-on:click="addValue(question)">Add Value</button>
                     </div>
-                    <div v-if="question.answerType === 'checkbox'">
+                    <div v-if="question.type === 'checkbox'">
                         <label for="">Check Box Values</label>
+                        <button v-on:click="addValue(question)">Add Value</button>
                         <div v-for="val in question.values">
                             <input class="form-control" type="text" v-model="val.value" required>
                         </div>
-                        <button v-on:click="addValue(question)">Add Value</button>
                     </div>
                 </div>
                 <div class="col-md-2"></div>
@@ -56,8 +56,11 @@
         <form v-bind:action="action" method="post">
             <input name="json-values" type="text" v-bind:value="JSON.stringify(questionnaire)">
             <input type="hidden" name="_token" v-bind:value="csrf_token">
-            <button>Submit</button>
+            <button class="btn btn-primary pull-right" type="submit">Submit</button>
         </form>
+
+        <hr>
+        <h5 class="page-header">For development (JSON passed to controller)</h5>
         <pre>{{ questionnaire }}</pre>
 
     </div>
@@ -77,8 +80,8 @@
             addQuestion(){
                 this.questionnaire.questions.push({
                     question: '',
-                    isRequired: false,
-                    answerType: '',
+                    required: false,
+                    type: '',
                     values: []
                 });
             },
