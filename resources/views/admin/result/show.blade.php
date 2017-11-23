96d4011a716516ef60fa34e81634405381641888
deanearl <deanfailed@gmail.com>
2017-11-23 23:31:11 +0800

    Added static data for M&E

--- resources/views/admin/result/show.blade.php
+++ resources/views/admin/result/show.blade.php
@@ -11,6 +11,30 @@
             <h3>Question: lorem ipsum </h3>
         <div id="test"></div>
         </div>
+
+        <div>
+            <h3>Question: Are you in favor of the Non-Smoking Ordinance?Why?</h3>
+            <div id="Q2:">
+                <ol>
+                    <li>No, Because I regularly smoke when i have the sudden urge to smoke</li>
+                    <li>Yes, Because I get irritated when I smell the smoke of the cigarette </li>
+                </ol>
+            </div>
+        </div>
+
+        <div>
+            <h3> Question: Are you Against Smoking? </h3>
+            <div id="Q3">
+
+            </div>
+        </div>
+
+        <div>
+            <h3> Question: If you do Smoke How many sticks do you smoke?</h3>
+            <div id="Q4">
+
+            </div>
+        </div>
     </div>
 @endsection
 
@@ -58,7 +82,105 @@
                     selected: true
                 }]
             }]// ^^ points
-
         });
+
+        Highcharts.chart('Q3', {
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: ' '
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>({point.percentage:.1f}%)</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true
+                }
+            },
+            series: [{
+                name: 'Percent',
+                colorByPoint: true,
+                data: [{
+                    name: 'Yes',
+                    y: 76,
+                    selected: true
+                }, {
+                    name: 'No',
+                    y: 24,
+                    sliced: true,
+                    selected: true
+                }]
+            }]
+        });
+
+        Highcharts.chart('Q4', {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: ' '
+            },
+            subtitle: {
+                text: ' '
+            },
+            xAxis: {
+                categories: [
+                    'Number of Sticks'
+                ],
+                crosshair: true
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'number of respondents'
+                }
+            },
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} sticks</b></td></tr>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },
+            series: [{
+                name: '1 - 3',
+                data: [15]
+
+            }, {
+                name: '4 - 7',
+                data: [40]
+
+            }, {
+                name: '8 - 11',
+                data: [30]
+
+            }, {
+                name: '12 or more',
+                data: [45]
+
+            }]
+        });
+        // 625 respondents
+    </script>
+
+    <script>
+
     </script>
 @endsection
--- resources/views/public/monitorAndEval.blade.php
+++ resources/views/public/monitorAndEval.blade.php
@@ -1,4 +1,50 @@
 @extends('layouts.public')
+
 @section('content')
-    This is the Monitoring and Evaluation page.
+<div class="container">
+    <div class="col-md-12">
+        <form>
+                <div class="form-group">
+                    <label for="email"> Q1: Where do you Smoke?</label>
+                    <input type="text" class="form-control" id="email">
+                </div>
+
+                <div class="form-group">
+                    <label for="email"> Q2: Are you in favor of the Non-Smoking Ordinance? Why?</label>
+                    <textarea class="form-control" rows="5" id="comment"></textarea>
+                </div>
+
+            <div class="form-group">
+                <label for="email"> Q3: Are you Against Smoking?</label>
+                <div class="radio">
+                    <label><input type="radio" name="optradio">Yes</label>
+                </div>
+                <div class="radio">
+                    <label><input type="radio" name="optradio">No</label>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="email"> Q4: Why do you Smoke?</label>
+                <div class="checkbox">
+                    <label><input type="checkbox" value="">Feeling irritable‚ on edge‚ grouchy</label>
+                </div>
+                <div class="checkbox">
+                    <label><input type="checkbox" value="">Feeling down or sad</label>
+                </div>
+                <div class="checkbox">
+                    <label><input type="checkbox" value="">Having trouble sleeping</label>
+                </div>
+                <div class="checkbox">
+                    <label><input type="checkbox" value="">Feeling restless and jumpy</label>
+                </div>
+                <div class="checkbox">
+                    <label><input type="checkbox" value="">Having trouble thinking clearly and concentrating</label>
+                </div>
+            </div>
+
+            <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+    </div>
+</div>
 @endsection
