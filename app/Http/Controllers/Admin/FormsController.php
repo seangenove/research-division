f4ca9b78de2550c7b05332abe80bd746246513b0
Christopher Espiritu <christopheredrian@gmail.com>
2018-01-21 19:22:23 +0800

    Added transaction support for Create and Update operations on FormsController

--- app/Http/Controllers/Admin/FormsController.php
+++ app/Http/Controllers/Admin/FormsController.php
@@ -7,6 +7,7 @@ use App\Questionnaire;
 use App\Value;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class FormsController extends Controller
 {
@@ -29,33 +30,37 @@ class FormsController extends Controller
 
     public function store(Request $request)
     {
-        // Json object passed by the view
-        $questionnaire_object = json_decode($request->input('json-values'));
-        // dd($questionnaire_object);
-        $questionnaire = new Questionnaire();
-        $questionnaire->name = $questionnaire_object->name;
-        $questionnaire->description = $questionnaire_object->description;
-        $questionnaire->saveOrFail();
-        foreach ($questionnaire_object->questions as $q) {
-            $new_question = new Question();
-            $new_question->question = $q->question;
-            $new_question->required = $q->required ? 1 : 0;
-            $new_question->type = $q->type;
-            $new_question->questionnaire_id = $questionnaire->id;
-            $new_question->saveOrFail();
-            // If type is checkbox/radio
-            if ($q->type === 'radio' || $q->type === 'checkbox') {
-                // For each of the values
-                foreach ($q->values as $v) {
-                    $new_val = new Value();
-                    $new_val->value = $v->value;
-                    $new_val->question_id = $new_question->id;
-                    $new_val->saveOrFail();
-                    $new_question->values()->save($new_val);
+
+        DB::transaction(function () use ($request) {
+            // Json object passed by the view
+            $questionnaire_object = json_decode($request->input('json-values'));
+            // dd($questionnaire_object);
+            $questionnaire = new Questionnaire();
+            $questionnaire->name = $questionnaire_object->name;
+            $questionnaire->description = $questionnaire_object->description;
+            $questionnaire->saveOrFail();
+            foreach ($questionnaire_object->questions as $q) {
+                $new_question = new Question();
+                $new_question->question = $q->question;
+                $new_question->required = $q->required ? 1 : 0;
+                $new_question->type = $q->type;
+                $new_question->questionnaire_id = $questionnaire->id;
+                $new_question->saveOrFail();
+                // If type is checkbox/radio
+                if ($q->type === 'radio' || $q->type === 'checkbox') {
+                    // For each of the values
+                    foreach ($q->values as $v) {
+                        $new_val = new Value();
+                        $new_val->value = $v->value;
+                        $new_val->question_id = $new_question->id;
+                        $new_val->saveOrFail();
+                        $new_question->values()->save($new_val);
+                    }
                 }
+                $questionnaire->questions()->save($new_question);
             }
-            $questionnaire->questions()->save($new_question);
-        }
+        });
+
         return redirect('/admin/forms');
     }
 
@@ -65,96 +70,107 @@ class FormsController extends Controller
      */
     public function show($id)
     {
-        $questionnaire = Questionnaire::findOrFail($id);
-        $questionnaire_json= new \stdClass();
-        $questionnaire_json->name = $questionnaire->name;
-        $questionnaire_json->description = $questionnaire->description;
-        $questionnaire_json->questions = [];
-        $temp = [];
-        foreach ($questionnaire->questions as $q){
-            $temp_question = new \stdClass();
-            $temp_question->question = $q->question;
-            $temp_question->required= ($q->required === 1 ? true : false);
-            $temp_question->type = $q->type;
-            $temp_question->values = [];
-            if ($temp_question->type === 'checkbox' || $temp_question->type === 'radio'){
-                foreach ($q->values as $v){
-                    $vc = new \stdClass();
-                    $vc->value = $v->value;
-                    $temp_question->values[] = $vc;
+        DB::transaction(function () use ($id) {
+            $questionnaire = Questionnaire::findOrFail($id);
+            $questionnaire_json = new \stdClass();
+            $questionnaire_json->name = $questionnaire->name;
+            $questionnaire_json->description = $questionnaire->description;
+            $questionnaire_json->questions = [];
+            $temp = [];
+            foreach ($questionnaire->questions as $q) {
+                $temp_question = new \stdClass();
+                $temp_question->question = $q->question;
+                $temp_question->required = ($q->required === 1 ? true : false);
+                $temp_question->type = $q->type;
+                $temp_question->values = [];
+                if ($temp_question->type === 'checkbox' || $temp_question->type === 'radio') {
+                    foreach ($q->values as $v) {
+                        $vc = new \stdClass();
+                        $vc->value = $v->value;
+                        $temp_question->values[] = $vc;
+                    }
                 }
+                $temp[] = $temp_question;
             }
-            $temp[] = $temp_question;
-        }
-        $questionnaire_json->questions = $temp;
-        return view('forms.show', [
-            'questionnaire' => $questionnaire,
-            'questionnaire_json' => json_encode($questionnaire_json)
-        ]);
+            $questionnaire_json->questions = $temp;
+            return view('forms.show', [
+                'questionnaire' => $questionnaire,
+                'questionnaire_json' => json_encode($questionnaire_json)
+            ]);
+        });
     }
 
     public function edit($id)
     {
-        $questionnaire = Questionnaire::findOrFail($id);
-        $questionnaire_json= new \stdClass();
-        $questionnaire_json->name = $questionnaire->name;
-        $questionnaire_json->description = $questionnaire->description;
-        $questionnaire_json->questions = [];
-        $temp = [];
-        foreach ($questionnaire->questions as $q){
-            $temp_question = new \stdClass();
-            $temp_question->question = $q->question;
-            $temp_question->required= ($q->required === 1 ? true : false);
-            $temp_question->type = $q->type;
-            $temp_question->values = [];
-            if ($temp_question->type === 'checkbox' || $temp_question->type === 'radio'){
-                foreach ($q->values as $v){
-                    $vc = new \stdClass();
-                    $vc->value = $v->value;
-                    $temp_question->values[] = $vc;
+        DB::transaction(function () use ($id) {
+
+            $questionnaire = Questionnaire::findOrFail($id);
+            $questionnaire_json = new \stdClass();
+            $questionnaire_json->name = $questionnaire->name;
+            $questionnaire_json->description = $questionnaire->description;
+            $questionnaire_json->questions = [];
+            $temp = [];
+            foreach ($questionnaire->questions as $q) {
+                $temp_question = new \stdClass();
+                $temp_question->question = $q->question;
+                $temp_question->required = ($q->required === 1 ? true : false);
+                $temp_question->type = $q->type;
+                $temp_question->values = [];
+                if ($temp_question->type === 'checkbox' || $temp_question->type === 'radio') {
+                    foreach ($q->values as $v) {
+                        $vc = new \stdClass();
+                        $vc->value = $v->value;
+                        $temp_question->values[] = $vc;
+                    }
                 }
+                $temp[] = $temp_question;
             }
-            $temp[] = $temp_question;
-        }
-        $questionnaire_json->questions = $temp;
-        return view('forms.edit', [
-            'questionnaire' => $questionnaire,
-            'questionnaire_json' => json_encode($questionnaire_json)
-        ]);
+            $questionnaire_json->questions = $temp;
+            return view('forms.edit', [
+                'questionnaire' => $questionnaire,
+                'questionnaire_json' => json_encode($questionnaire_json)
+            ]);
+        });
     }
 
-    public function update(Request $request, $id){
-        // Json object passed by the view
-        $questionnaire_object = json_decode($request->input('json-values'));
-        // dd($questionnaire_object);
-        $questionnaire = Questionnaire::findOrFail($id);
-        $questionnaire->name = $questionnaire_object->name;
-        $questionnaire->description = $questionnaire_object->description;
-        $questionnaire->saveOrFail();
-        foreach ($questionnaire->questions as $q){
-            $q->delete();
-        }
-        foreach ($questionnaire_object->questions as $q) {
-            $new_question = new Question();
-            $new_question->question = $q->question;
-            $new_question->required = $q->required ? 1 : 0;
-            $new_question->type = $q->type;
-            $new_question->questionnaire_id = $questionnaire->id;
-            $new_question->saveOrFail();
-            // If type is checkbox/radio
-            if ($q->type === 'radio' || $q->type === 'checkbox') {
-                // For each of the values
-                foreach ($q->values as $v) {
-                    $new_val = new Value();
-                    $new_val->value = $v->value;
-                    $new_val->question_id = $new_question->id;
-                    $new_val->saveOrFail();
-                    $new_question->values()->save($new_val);
-                }
+    public function update(Request $request, $id)
+    {
+        DB::transaction(function () use ($id, $request) {
+
+            // Json object passed by the view
+            $questionnaire_object = json_decode($request->input('json-values'));
+            // dd($questionnaire_object);
+            $questionnaire = Questionnaire::findOrFail($id);
+            $questionnaire->name = $questionnaire_object->name;
+            $questionnaire->description = $questionnaire_object->description;
+            $questionnaire->saveOrFail();
+            foreach ($questionnaire->questions as $q) {
+                $q->delete();
             }
-            $questionnaire->questions()->save($new_question);
-        }
+            foreach ($questionnaire_object->questions as $q) {
+                $new_question = new Question();
+                $new_question->question = $q->question;
+                $new_question->required = $q->required ? 1 : 0;
+                $new_question->type = $q->type;
+                $new_question->questionnaire_id = $questionnaire->id;
+                $new_question->saveOrFail();
+                // If type is checkbox/radio
+                if ($q->type === 'radio' || $q->type === 'checkbox') {
+                    // For each of the values
+                    foreach ($q->values as $v) {
+                        $new_val = new Value();
+                        $new_val->value = $v->value;
+                        $new_val->question_id = $new_question->id;
+                        $new_val->saveOrFail();
+                        $new_question->values()->save($new_val);
+                    }
+                }
+                $questionnaire->questions()->save($new_question);
+            }
+            return redirect('/admin/forms');
+        });
         return redirect('/admin/forms');
     }
+
 
 }
