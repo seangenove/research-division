48d9f73f17ef3124359af09470026a3ac8edf477
Christopher Espiritu <christopheredrian@gmail.com>
2018-01-25 17:26:08 +0800

    Added reports for answers

--- app/Http/Controllers/Admin/ResultController.php
+++ app/Http/Controllers/Admin/ResultController.php
@@ -7,6 +7,8 @@ use DB;
 use App\Answer;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Maatwebsite\Excel\Facades\Excel;
+use Psy\Exception\ErrorException;
 
 class ResultController extends Controller
 {
@@ -21,24 +23,24 @@ class ResultController extends Controller
     {
 
         $allAnswers = DB::table('answers')
-            ->join('responses','answers.response_id' , '=' , 'responses.id')
-            ->where('responses.questionnaire_id' , '=' ,$id)
+            ->join('responses', 'answers.response_id', '=', 'responses.id')
+            ->where('responses.questionnaire_id', '=', $id)
             ->get();
 
         $allQuestions = DB::table('answers')
-            ->join('responses','answers.response_id' , '=' , 'responses.id')
-            ->join('questions', 'responses.questionnaire_id' ,'=', 'questions.questionnaire_id')
-            ->where('responses.questionnaire_id' , '=' ,$id)
+            ->join('responses', 'answers.response_id', '=', 'responses.id')
+            ->join('questions', 'responses.questionnaire_id', '=', 'questions.questionnaire_id')
+            ->where('responses.questionnaire_id', '=', $id)
             ->get();
 
- //    dd($allAnswers);
+        //    dd($allAnswers);
 //        dd($allQuestions);
 
 
         // NOTE: An alternative for the above query
         $questionnaire = Questionnaire::find($id);
         $allQuestions = $questionnaire->questions;
-        foreach($allQuestions as $question){
+        foreach ($allQuestions as $question) {
             $answers = $question->answers;
             // Echo Question name
             echo "Question: " . $question->question . "<br>";
@@ -52,7 +54,46 @@ class ResultController extends Controller
 
         dd();
         return view('admin.result.show')
-            ->with('answers',$allAnswers)
-            ->with('questions',$allQuestions);
+            ->with('answers', $allAnswers)
+            ->with('questions', $allQuestions);
+    }
+
+    public function downloadExcel($id)
+    {
+        try {
+            Excel::create('file', function ($excel) use ($id) {
+                $excel->sheet('Excel sheet', function ($sheet) use ($id) {
+                    $questions_arr = [];
+                    $answers_arr = [];
+                    $count = 0;
+                    $questionnaire = Questionnaire::find($id);
+                    foreach ($questionnaire->questions as $question) {
+                        $questions_arr[] = $question->question;
+                        foreach ($question->answers as $answer) {
+                            $answers_arr[$count][] = $answer->answer;// 0:
+                        }
+                        $count += 1;
+                    }
+                    $sheet->appendRow($questions_arr);
+                    $rows = [];
+                    for ($x = 0; $x < count($answers_arr); $x++) {
+                        for ($y = 0; $y < count($answers_arr[$x]); $y++) {
+                            $rows[$y][$x] = $answers_arr[$x][$y];
+                        }
+                    }
+                    foreach ($rows as $row) {
+                        $sheet->appendRow($row);
+                    }
+
+//                dd($rows);
+
+                    $sheet->setOrientation('landscape');
+
+                });
+            })->export('xls');
+        } catch (\Exception $e) {
+            dd('Invalid query....');
+        }
+
     }
 }
